550bdd6dbb6c939b2be88e01dacf34aeaafe5ea3
elvio.junior <elvio.junior@softplan.com.br>
2021-06-10 16:04:41 -0300

    Ajustes

--- src/Inpulse.Autentication.WebApi/Startup.cs
+++ src/Inpulse.Autentication.WebApi/Startup.cs
@@ -1,5 +1,7 @@
+using Inpulse.Autentication.WebApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,6 +22,9 @@ namespace Inpulse.Autentication.WebApi
         {
 
             services.AddControllers();
+            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("DataBase"));
+            
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Inpulse.Autentication.WebApi", Version = "v1" });
